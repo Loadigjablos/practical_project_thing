b2ff7dec7a5229a2aac05b8c9a24a827f55b0c18
Loadigjablos <loadingjablowme@gmail.com>
2023-05-16 16:02:58 +0200

    added screen

---
+++ client/controler/oneUsersFiles.js
@@ -0,0 +1,36 @@
+const thisOpenFile = document.querySelector('#this-file');
+
+const filesFromUser = document.querySelector('#files-from-user')
+
+
+
+
+const typeOfFile = document.querySelector('#type-of-file')
+const fileToSend = document.querySelector('#file-adding')
+
+document.querySelector('#add-file-to-user').addEventListener('click', async (e) => {
+
+});
+
+async function getAllFilesFromHashUser() {
+    const response = await fetch("/API/V1/Classes", {
+        method: "get",
+        cache: "no-cache",
+      });
+    
+      if (!response.ok) {
+        MessageUI(
+          "Failed",
+          "the user was not found or the server had an error"
+        );
+        return;
+      }
+      const dataThing = await response.json();
+    
+      dataThing.forEach((userThing) => {
+        addFirmToList(userThing);
+      });
+}
+
+
+
