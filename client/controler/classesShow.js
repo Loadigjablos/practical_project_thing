db78bb7157a4962e11e270a12758c0058d151d9d
Loadigjablos <loadingjablowme@gmail.com>
2023-05-16 14:48:40 +0200

    prettier

--- client/controler/classesShow.js
+++ client/controler/classesShow.js
@@ -1,4 +1,3 @@
-/*
 try {
     if (user.role !== "A") {
       document.querySelector('#only-admin').className = "hidden";
@@ -7,77 +6,78 @@ try {
     document.querySelector('#only-admin').className = "hidden";
     MessageUI("error", "you need to login")
 }
-*/
 
-const className = document.querySelector('#class-name')
-const classspecialization = document.querySelector('#class-specialization')
-const classyaer_qv = document.querySelector('#class-yaer_qv')
+const className = document.querySelector("#class-name");
+const classspecialization = document.querySelector("#class-specialization");
+const classyaer_qv = document.querySelector("#class-yaer_qv");
 
 function addFirmToList(data) {
-    const row = document.createElement('tr');
+  const row = document.createElement("tr");
 
-    const id = document.createElement('td');
-    id.innerText = data.id;
-    row.appendChild(id);
+  const id = document.createElement("td");
+  id.innerText = data.id;
+  row.appendChild(id);
 
-    const class_name = document.createElement('td');
-    class_name.innerText = data.class_name;
-    row.appendChild(class_name);
+  const class_name = document.createElement("td");
+  class_name.innerText = data.class_name;
+  row.appendChild(class_name);
 
-    const specialization = document.createElement('td');
-    specialization.innerText = data.specialization;
-    row.appendChild(specialization);
+  const specialization = document.createElement("td");
+  specialization.innerText = data.specialization;
+  row.appendChild(specialization);
 
-    const yaer_qv = document.createElement('td');
-    yaer_qv.innerText = data.yaer_qv;
-    row.appendChild(yaer_qv);
+  const yaer_qv = document.createElement("td");
+  yaer_qv.innerText = data.yaer_qv;
+  row.appendChild(yaer_qv);
 
-    firms.appendChild(row)
+  firms.appendChild(row);
 }
 
-document.querySelector('#create-class-').addEventListener('click', async (e) => {
-
+document
+  .querySelector("#create-class-")
+  .addEventListener("click", async (e) => {
     const data = {
-        class_name: className.value,
-        specialization: classspecialization.value,
-        yaer_qv: classyaer_qv.value
-    }
+      class_name: className.value,
+      specialization: classspecialization.value,
+      yaer_qv: classyaer_qv.value,
+    };
 
     const response = await fetch("/API/V1/Class", {
-        method: "post",
-        body: JSON.stringify(data),
-        cache: "no-cache",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        MessageUI("Failed", "Please Try Again")
-        return;
-      }
-      MessageUI("Created", "class Was created")
-
-    addFirmToList(data)
-});
-
-async function sdvfkshdvhksd() {
-
-    const response = await fetch("/API/V1/Classes", {
-        method: "get",
-        cache: "no-cache"
+      method: "post",
+      body: JSON.stringify(data),
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     if (!response.ok) {
-        MessageUI("Failed", "Please Connect to the internet or conntact the developer")
-        return;
-      }
-      const dataThing = await response.json();
-    
-      dataThing.forEach(userThing => {
-        addFirmToList(userThing)
-      });
-    
+      MessageUI("Failed", "Please Try Again or it already exists");
+      return;
+    }
+    MessageUI("Created", "class Was created");
+
+    addFirmToList(data);
+  });
+
+async function sdvfkshdvhksd() {
+  const response = await fetch("/API/V1/Classes", {
+    method: "get",
+    cache: "no-cache",
+  });
+
+  if (!response.ok) {
+    MessageUI(
+      "Failed",
+      "Please Connect to the internet or conntact the developer"
+    );
+    return;
+  }
+  const dataThing = await response.json();
+
+  dataThing.forEach((userThing) => {
+    addFirmToList(userThing);
+  });
 }
 
-sdvfkshdvhksd()
+sdvfkshdvhksd();
