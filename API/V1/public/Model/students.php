ddc5e361a4f4e7be7e9d5739a019ddd63cf96269
INF21-009\kaenu <kanusan1@hotmail.ch>
2023-06-15 15:43:24 +0200

    some correction: students.php

--- API/V1/public/Model/students.php
+++ API/V1/public/Model/students.php
@@ -52,38 +52,31 @@
     }
 
     function delete_student($student_id, $user_id) {
-
         global $database;
         $check = $database->query("SELECT user_id FROM students WHERE user_id = '$user_id';")->fetch_assoc()["user_id"];
-
+    
         if ($check === $user_id) {
-            $result = $database->query("DELETE FROM students WHERE student_id = '$student_id';");
-            if ($result) {
-                message_function(200, "User deleted successfully");
-            } else {
-                $roleCheck = $database->query("SELECT user_id FROM students WHERE student_id = '$student_id';")->fetch_assoc()["user_id"];
-                if (!$roleCheck) {
-                    error_function(400, "This student does not have an user");
-                }
-                $getRole = $database->query("SELECT role FROM user WHERE user_id = '$roleCheck';")->fetch_assoc()["role"];
-                if (!$getRole) {
-                    error_function(400, "There is no Role");
-                }
-                if ($getRole === "A") {
-                    $deleteUser = $database->query("DELETE FROM students WHERE student_id = '$student_id';");
-                    if ($result) {
-                        message_function(200, "User deleted successfully");
-                    }
-                    else {
-                        error_function(400,"Can't remove this user.");
-                    }
+            $hasLinks = $database->query("SELECT student_id FROM blob_files WHERE student_id = '$student_id';")->fetch_assoc()["student_id"];
+    
+            if ($hasLinks) {
+                $unlinkResult = $database->query("DELETE FROM blob_files WHERE student_id = '$student_id';");
+                if (!$unlinkResult) {
+                    error_function(500, "error: 'blob_files'");
+                    return;
                 }
             }
+
+            $deleteResult = $database->query("DELETE FROM students WHERE student_id = '$student_id';");
+            if ($deleteResult) {
+                message_function(200, "successfully deleted");
+            } else {
+                error_function(500, "error");
+            }
+        } else {
+            error_function(400, "access denied");
         }
-        else if ($check !== $student_id) {
-            error_function(400, "Access denied");
-        }       
     }
+    
 
     /**
      * function delete_student($student_id, $user_id) {
